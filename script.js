7636e8a3af837fb92112cbf3d967b24fa8ee34db
Mariellen29 <mariellenguile@gmail.com>
2023-01-21 19:42:59 -0300

    arrumado

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
-button,addEventListener('click', add)
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+
 
 function add() {
 const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
 const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
-   alert ("dia já incluso 🔴")
+    alert("dia já incluso 🔴")
+    return
  }
   
   
